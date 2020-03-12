de3823d15fe8318c9c93ea2eec2fb8bf28e3ebf8
Myousry <mohamedyousry250@gmail.com>
2020-03-12 20:09:54 +0200

    edit and delete the posts funcs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
-    
+    before_action :find_post , only: [:show , :edit , :update , :destroy]
     def index 
         @posts = Post.all.order("created_at DESC")
     end
     def show
-        @post = Post.find(params[:id])
+        
     end
 
     def new 
@@ -21,6 +21,26 @@ class PostsController < ApplicationController
 
     end
 
+    def edit 
+    
+    end
+
+    def update
+
+        if (@post.update(post_params))
+            redirect_to @post 
+        else
+            render 'edit'
+        end
+
+    end
+
+    def destroy 
+        @post.destroy
+        redirect_to root_path
+    end
+        
+
 
 
 
@@ -30,6 +50,10 @@ class PostsController < ApplicationController
 
         def post_params
             params.require(:post).permit(:title,:body)
+        end
+
+        def find_post
+            @post = Post.find(params[:id])
         end
 
 end
